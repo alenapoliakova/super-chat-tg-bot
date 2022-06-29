69b095e6cebb94e1bff37ae83516e7ef0424f329
Alena <a.polyackowa2016@gmail.com>
2022-06-29 22:28:43 +0300

    update chat bot: fix send arcticle in group

--- src/bot.py
+++ src/bot.py
@@ -1,41 +1,38 @@
 import telebot
-import requests
 import logging
 import coloredlogs
-from bs4 import BeautifulSoup
 
 from parsing_habr import parse_habr
 from check_env import BOT_TOKEN, BOT_NAME
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.NOTSET,
     format="%(asctime)s %(levelname)s %(message)s",
     handlers=[logging.StreamHandler(), logging.FileHandler(filename="../bot.log")],
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 coloredlogs.install()
 
-
-url = "https://habr.com/ru/top/daily/"
-resp = requests.get(url).text
-soup = BeautifulSoup(resp, "html.parser")
-
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode="Markdown")
 logging.info(f"BOT {BOT_NAME} STARTED")
 
 
 @bot.message_handler(content_types=["new_chat_members"])
 def answer_to_a_new_member(message):
-    bot.reply_to(message, f"Привет, {message.from_user.first_name}, добро пожаловать в наш чат!\n\n")
+    logging.info(f"ADD USER {message.from_user.username}")
+    if name := message.from_user.first_name:
+        bot.reply_to(message, f"Привет, {name}, добро пожаловать в наш чат!")
+    else:
+        bot.reply_to(message, f"Привет, добро пожаловать в наш чат!")
 
 
-@bot.message_handler(commands=["/random_habr"])
+@bot.message_handler(content_types=["text"], commands=["random_habr"])
 def send_random_article_from_habr(message):
     """Отправить пользователю рандомную статью из топа за день на Хабре"""
+    logging.info(f"RECV {message.text} FROM {message.from_user.username}")
     habr_article = parse_habr()
-    bot.reply_to(message.chat.id, f"<b>Статья</b> {habr_article.title}\n\n"
-                                  f"Ссылка на статью: <a href='{habr_article.url}'>Клик</a>")
+    bot.reply_to(message, f"_{habr_article.title}_\n\nСсылка: [клик]({habr_article.url})")
 
 
 bot.polling(none_stop=True)
